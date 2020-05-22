f2d9c6783df84829ef42e73a4d6accb07b24e067
dejantrtnik <dejantrtnik@gmail.com>
2020-05-22 09:08:26 +0200

    Realease

---
+++ db_conn.php
@@ -0,0 +1,8 @@
+<?php
+$conn = mysqli_connect('DB_CONN', 'USER', 'PASS', 'DB');
+mysqli_set_charset($conn,"utf8");
+if (!$conn)
+  {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+?>
